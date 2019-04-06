a35fe62938cafc9c5be24515dfcfa466cb390c6d
Luis Sobrinho <ads.luis.sobrinho@gmail.com>
2019-04-06 08:24:01 -0300

    Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,47 +1,229 @@
-@extends('layouts.master')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <title>{{ config('app.name', 'Laravel') }} | Registro</title>
+    <!-- HTML5 Shim and Respond.js IE10 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 10]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="Admindek Bootstrap admin template made using Bootstrap 4 and it has huge amount of ready made feature, UI components, pages which completely fulfills any dashboard needs." />
+    <meta name="keywords" content="bootstrap, bootstrap admin template, admin theme, admin dashboard, dashboard template, admin template, responsive" />
+    <meta name="author" content="colorlib" />
+    <!-- Favicon icon -->
 
-@section('app-content')
+    <link rel="icon" href="https://colorlib.com//polygon/admindek/files/assets/images/favicon.ico" type="image/x-icon">
+    <!-- Google font-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:500,700" rel="stylesheet">
+    <!-- Required Fremwork -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('bower_components/bootstrap/css/bootstrap.min.css') }}">
+    <!-- waves.css -->
+    <link rel="stylesheet" href="{{ asset('assets/pages/waves/css/waves.min.css') }}" type="text/css" media="all">
+    <!-- feather icon -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/icon/feather/css/feather.css') }}">
+    <!-- themify-icons line icon -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/icon/themify-icons/themify-icons.css') }}">
+    <!-- ico font -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/icon/icofont/css/icofont.css') }}">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/icon/font-awesome/css/font-awesome.min.css') }}">
+    <!-- Style.css -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/pages.css') }}">
+</head>
 
-        <div class="form-small">
-
-        <h2 class="text-center">Register</h2>
-
-        <form method="POST" action="/register">
-            {!! csrf_field() !!}
-            <div class="row">
-                <div class="col-md-12 raw-margin-top-24">
-                    <label>Name</label>
-                    <input class="form-control" type="text" name="name" value="{{ old('name') }}" placeholder="Name">
+<body themebg-pattern="theme1">
+<!-- Pre-loader start -->
+<div class="theme-loader">
+    <div class="loader-track">
+        <div class="preloader-wrapper">
+            <div class="spinner-layer spinner-blue">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="gap-patch">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-12 raw-margin-top-24">
-                    <label>Email</label>
-                    <input class="form-control" type="email" name="email" value="{{ old('email') }}" placeholder="Email">
+            <div class="spinner-layer spinner-red">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="gap-patch">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-12 raw-margin-top-24">
-                    <label>Password</label>
-                    <input class="form-control" type="password" name="password" placeholder="Password">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12 raw-margin-top-24">
-                    <label>Confirm Password</label>
-                    <input class="form-control" type="password" name="password_confirmation" placeholder="Password Confirmation">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12 raw-margin-top-24">
-                    <div class="btn-toolbar justify-content-between">
-                        <button class="btn btn-primary" type="submit">Register</button>
-                        <a class="btn btn-link" href="/login">Login</a>
-                    </div>
-                </div>
-            </div>
-        </form>
 
+            <div class="spinner-layer spinner-yellow">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="gap-patch">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
+                </div>
+            </div>
+
+            <div class="spinner-layer spinner-green">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="gap-patch">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
+<!-- Pre-loader end -->
+<section class="login-block">
+    <!-- Container-fluid starts -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <!-- Authentication card start -->
+                <form class="md-float-material form-material" action="{{ route('login') }}" method="POST">
+                    @csrf
+                    <div class="text-center">
+                        <img src="{{ asset('assets/images/logo.png') }}" alt="logo.png">
+                    </div>
+                    <div class="auth-box card">
+                        <div class="card-block">
+                            <div class="row m-b-20">
+                                <div class="col-md-12">
+                                    <h3 class="text-center txt-primary">Sign up</h3>
+                                </div>
+                            </div>
+                            <p class="text-muted text-center p-b-5">Crie sua conta regular</p>
 
-@stop
+                            <div class="text-center">
+                                <img src="{{ asset('assets/images/logo.png') }}" alt="logo.png">
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-md-12 raw-margin-top-24">
+                                    <label>Name</label>
+                                    <input class="form-control" type="text" name="name" value="{{ old('name') }}" placeholder="Name">
+                                </div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-md-12 raw-margin-top-24">
+                                    <label>Email</label>
+                                    <input class="form-control" type="email" name="email" value="{{ old('email') }}" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-md-12 raw-margin-top-24">
+                                    <label>Password</label>
+                                    <input class="form-control" type="password" name="password" placeholder="Password">
+                                </div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-md-12 raw-margin-top-24">
+                                    <label>Confirm Password</label>
+                                    <input class="form-control" type="password" name="password_confirmation" placeholder="Password Confirmation">
+                                </div>
+                            </div>
+                            <div class="row mt-4">
+                                <div class="col-md-12 raw-margin-top-24">
+                                    <div class="btn-toolbar justify-content-between">
+                                        <button class="btn btn-primary" type="submit">Register</button>
+                                        <a class="btn btn-link" href="/login">Login</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <!-- end of form -->
+            </div>
+            <!-- Authentication card end -->
+        </div>
+        <!-- end of col-sm-12 -->
+    </div>
+    <!-- end of row -->
+    </div>
+    <!-- end of container-fluid -->
+</section>
+<!-- Warning Section Starts -->
+<!-- Older IE warning message -->
+<!--[if lt IE 10]>
+<div class="ie-warning">
+    <h1>Warning!!</h1>
+    <p>You are using an outdated version of Internet Explorer, please upgrade
+        <br/>to any of the following web browsers to access this website.
+    </p>
+    <div class="iew-container">
+        <ul class="iew-download">
+            <li>
+                <a href="http://www.google.com/chrome/">
+                    <img src="{{ asset('assets/images/browser/chrome.png') }}" alt="Chrome">
+                    <div>Chrome</div>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.mozilla.org/en-US/firefox/new/">
+                    <img src="{{ asset('assets/images/browser/firefox.png') }}" alt="Firefox">
+                    <div>Firefox</div>
+                </a>
+            </li>
+            <li>
+                <a href="http://www.opera.com">
+                    <img src="{{ asset('assets/images/browser/opera.png') }}" alt="Opera">
+                    <div>Opera</div>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.apple.com/safari/">
+                    <img src="{{ asset('assets/images/browser/safari.png') }}" alt="Safari">
+                    <div>Safari</div>
+                </a>
+            </li>
+            <li>
+                <a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie">
+                    <img src="{{ asset('assets/images/browser/ie.png') }}" alt="">
+                    <div>IE (9 & above)</div>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <p>Sorry for the inconvenience!</p>
+</div>
+<![endif]-->
+<!-- Warning Section Ends -->
+<!-- Required Jquery -->
+<script type="text/javascript" src="{{ asset('bower_components/jquery/js/jquery.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('bower_components/jquery-ui/js/jquery-ui.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('bower_components/popper.js/js/popper.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('bower_components/bootstrap/js/bootstrap.min.js') }}"></script>
+<!-- waves js -->
+<script src="{{ asset('assets/pages/waves/js/waves.min.js') }}"></script>
+<!-- jquery slimscroll js -->
+<script type="text/javascript" src="{{ asset('bower_components/jquery-slimscroll/js/jquery.slimscroll.js') }}"></script>
+<script src="{{ asset('assets/js/pcoded.min.js') }}"></script>
+<script src="{{ asset('assets/js/vertical/vertical-layout.min.js') }}"></script>
+<!-- notification js -->
+<script type="text/javascript" src="{{ asset('assets/js/bootstrap-growl.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/pages/notification/notification.js') }}"></script>
+<!-- Custom js -->
+<script type="text/javascript" src="{{ asset('assets/js/script.min.js') }}"></script>
+@include('partials.message')
+@include('partials.errors')
+</body>
+</html>
